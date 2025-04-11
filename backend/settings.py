dc03320cc861f6d3cbeaede96f8a5207ae63729f
Owen <freedomcoding101@gmail.com>
2025-04-10 21:38:51 -0600

    changed settings to push to railway

--- backend/settings.py
+++ backend/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14t414krdoz#9#iwb+wm&oyzod3@a+t!r$4pk@-sg*nji5k9h5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "*",
+
+]
 
 
 # Application definition
